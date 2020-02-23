2398c84956ca74f248de33100a616d29011c5a44
Artur Moczulski <artur.moczulski@gmail.com>
2020-02-23 01:19:25 -0800

    feature(layout): latest elevated layout and content

--- config/environments/development.php
+++ config/environments/development.php
@@ -3,3 +3,15 @@
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
+
+ini_set('log_errors','On');
+
+ini_set('display_errors','Off');
+
+ini_set('error_reporting', E_ALL );
+
+define('WP_DEBUG', false);
+
+define('WP_DEBUG_LOG', true);
+
+define('WP_DEBUG_DISPLAY', false);
